1aab3b65406ee61f22c0afa475c4a7ea027f3652
Shay <fwasil83@gmail.com>
2023-07-20 01:51:15 -0400

    venues functionality

--- frontend/src/components/Pages/Venues.jsx
+++ frontend/src/components/Pages/Venues.jsx
@@ -1,45 +1,82 @@
-import { useState, useEffect } from "react"
-import { Container, Row, Col } from "react-bootstrap"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import axios from "axios";
+import VenueDetails from "./VenueDetails";
 
-function Venues(){
+function Venues() {
+  const [venues, setVenues] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [selectedVenue, setSelectedVenue] = useState(null);
 
-    const [venues, setVenues] = useState(null)
+  useEffect(() => {
+    const getVenues = async () => {
+      try {
+        const venueAPI = await axios.get("http://54.193.32.199:8000/venues/");
+        setVenues(venueAPI.data);
+        setLoading(false);
+      } catch (error) {
+        setError("Error fetching venues. Please try again later.");
+        setLoading(false);
+      }
+    };
+    getVenues();
+  }, []);
 
-    useEffect(()=>{
-        const getVenues = async() => {
-            const venueAPI = await axios.get('http://54.193.32.199:8000/venues/')
-            console.log(venueAPI.data)
-            setVenues(venueAPI.data)
-            return venueAPI.data
-        }
-        getVenues()
-    }, [])
+  const handleVenueClick = (venue) => {
+    setSelectedVenue((prevSelectedVenue) => (prevSelectedVenue && prevSelectedVenue.id === venue.id) ? null : venue);
+  };
 
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
+  if (error) {
+    return <p>{error}</p>;
+  }
 
-    return(
-        <div>
-            <Container className='py-5'>
-                <Row xl={3} md={2} sm={1} style={{justifyContent: `space-evenly`, gap: `1rem`}}>
-                    {venues && venues.map((venue, index)=>(
-                        <Col>
-                            <Container className='my-4 p-0' key={index} style={{borderRadius: `50px`,
-                                background: `#e0e0e0`,
-                                boxShadow:  `20px 20px 60px #bebebe, -20px -20px 60px #ffffff`,
-                                overflow: 'hidden'}}
-                                >
-                                    <img src={venue.picture_link} style={{width: '100%', maxHeight: `300px`, aspectRatio: '16/9'}}></img>
-                                    <h1 className='pt-3'>{venue.name}</h1>
-                                    <p>{venue.address}, {venue.city}</p>
-                                    <p>{venue.state}, {venue.country}</p>
-                            </Container> 
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-        </div>
-    )
+  return (
+    <div>
+      <Container className="py-5">
+        <Row xl={3} md={2} sm={1} style={{ justifyContent: `space-evenly`, gap: `1rem`, }}>
+          {venues && venues.length > 0 ? (
+            venues.map((venue) => (
+              <Col key={venue.id}>
+                <div style={{ cursor: "pointer" }}>
+                  <Container
+                    className="my-4 p-0"
+                    style={{
+                      borderRadius: `50px`,
+                      background: `#e0e0e0`,
+                      boxShadow: `20px 20px 60px #bebebe, -20px -20px 60px #ffffff`,
+                      overflow: "hidden",
+                    }}
+                    onClick={() => handleVenueClick(venue)}
+                  >
+                    <img
+                      src={venue.picture_link}
+                      style={{ width: "100%", maxHeight: `300px`, aspectRatio: "16/9" }}
+                      alt="Venue"
+                    />
+                    <h1 className="pt-3">{venue.name}</h1>
+                    <p>
+                      {venue.address}, {venue.city}
+                    </p>
+                    <p>
+                      {venue.state}, {venue.country}
+                    </p>
+                  </Container>
+                </div>
+                {selectedVenue && selectedVenue.id === venue.id && <VenueDetails venue={selectedVenue} />}
+              </Col>
+            ))
+          ) : (
+            <p>No venues found.</p>
+          )}
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Venues
+export default Venues;
